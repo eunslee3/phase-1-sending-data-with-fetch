b158f1ff1499f8f93b7f89cb9552e70aeff8f13a
Samuel <lee.eunseong7856@gmail.com>
2022-11-03 10:49:28 -0500

    commit tree df4316a4eff842790852c8f15e87a6a9c61fe81c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const submitData = (name, email) => {
+    return fetch("http://localhost3000/users", {
+        method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+    });
+}
+
+// const configurationData = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name: "Steve",
+//         email: "steve@steve.com"
+//     })
+// }
